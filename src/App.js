db082e2ba0817f7801375b755b1321171d3fa1ce
jasperwreed <jmixon.reed@gmail.com>
2020-11-14 16:07:36 -0800

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <h2>Jasper Reed</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home
